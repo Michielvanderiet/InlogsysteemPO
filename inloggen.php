40d0064df7f0e505ab90607b0c0cfe69ffe3bcda
Wouter de Vries <vriesdewouterj@gmail.com>
2018-12-07 14:12:46 +0100

    Add files via upload

--- inloggen.php
+++ inloggen.php
@@ -4,11 +4,13 @@
 </head>
 <body>
 <!-- veel korter dan dit kan niet //-->
+<div id="inlogbox">
 <h2>Inloggen</h2>
 <form action="menu.php" method="post">
 Gebruikersnaam: <input type="text" name="usernaam"><br />
 Wachtwoord: <input type="password" name="wachtwoord"><br />
 <input type="submit" name="Inloggen" value="Inloggen">
 </form>
+</div>
 </body>
 </html>
